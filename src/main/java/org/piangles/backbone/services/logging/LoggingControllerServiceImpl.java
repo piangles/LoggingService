64c78957e6a1d3e5ee4abc9be5f4206ead951ce3
saradhivs <saradhivs@yahoo.com>
2020-12-17 08:57:03 -0500

    Fixed the bug for endpoint lookup.

--- src/main/java/org/piangles/backbone/services/logging/LoggingControllerServiceImpl.java
+++ src/main/java/org/piangles/backbone/services/logging/LoggingControllerServiceImpl.java
@@ -2,23 +2,40 @@ package org.piangles.backbone.services.logging;
 
 import java.lang.reflect.Method;
 
+import javax.management.RuntimeErrorException;
+
 import org.piangles.core.services.Request;
 import org.piangles.core.services.remoting.AbstractService;
 
 public class LoggingControllerServiceImpl extends AbstractService
 {
-	private LoggingServiceImpl loggingService = null;
+	private LoggingServiceImpl loggingServiceImpl = null;
+	private Method recordMethod = null;
 
-	public LoggingControllerServiceImpl(LoggingServiceImpl loggingService)
+	public LoggingControllerServiceImpl(LoggingServiceImpl loggingServiceImpl)
 	{
-		super(loggingService);
-		this.loggingService = loggingService;
+		super(loggingServiceImpl);
+		
+		this.loggingServiceImpl = loggingServiceImpl;
+		try
+		{
+			recordMethod = loggingServiceImpl.getClass().getMethod("record", LogEvent.class);
+		}
+		catch (Exception e)
+		{
+			throw new RuntimeErrorException(new Error(this.getClass().getCanonicalName()), e.getMessage());
+		}
 	}
 
+	protected Method lookupMethod(String endpoint, Object[] args)
+	{
+		return recordMethod;
+	}
+	
 	@Override
 	protected Object process(Method method, Object[] args, Request request) throws Exception
 	{
-		loggingService.record((LogEvent)args[0]);
+		loggingServiceImpl.record((LogEvent)args[0]);
 
 		return null;
 	}
